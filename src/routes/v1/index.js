8633b4bd98936feaadf190e06b4f0a1055956c1f
Raunak Scarlet <raunakscarlet@gmail.com>
2023-12-15 21:07:35 +0530

    get a tweet with params

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createTweet } from "../../controllers/tweet-controller.js";
+import { createTweet, getTweet } from "../../controllers/tweet-controller.js";
 import { toggleLike } from "../../controllers/like-controller.js";
 import { createComment } from "../../controllers/coment-controller.js";
 
 const router = express.Router();
+
+router.get("/tweets/:id", getTweet);
 router.post("/tweets", createTweet);
 
 router.post("/likes/toggle", toggleLike);
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -34,6 +34,11 @@ class TweetService {
 
         return tweet;
     }
+
+    async get(tweetId) {
+        const tweet = this.tweetRepository.getWithComments(tweetId);
+        return tweet;
+    }
 }
 
 export default TweetService;
